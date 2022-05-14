536ef1ed962e7652278b010aa77b09881d14fb48
Andrej susnik <susnik.andrej7@gmail.com>
2022-05-15 00:37:21 +0200

    Add device endpoints

--- backend_server/db.py
+++ backend_server/db.py
@@ -13,9 +13,18 @@ class Serial:
     def as_dict(self):
         return {c.name: str(getattr(self, c.name)) if getattr(self, c.name) else None for c in self.__table__.columns}
 
+class Device(base, Serial):
+    __tablename__ = "device"
+    id = Column(INTEGER, primary_key=True)
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    network_ssid = Column(String, nullable=False)
+    ip = Column(String, nullable=False)
+    id_user = Column(INTEGER, ForeignKey("user.id"))
+
 
 class User(base, Serial):
-    __tablename__ = "User"
+    __tablename__ = "user"
     id = Column(INTEGER, primary_key=True)
     username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
--- backend_server/main.py
+++ backend_server/main.py
@@ -1,11 +1,12 @@
 import hashlib
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
 import db
 import waitress
 import sys
-from flask_jwt_extended import create_access_token, jwt_required, JWTManager
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt_identity
 import os
+from flask_cors import CORS
 import datetime
 from dotenv import load_dotenv
 
@@ -13,8 +14,46 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.getenv('jwt_secret_key')
+CORS(app)
 jwt = JWTManager(app)
 api = Api(app)
+
+deviceParser = reqparse.RequestParser()
+deviceParser.add_argument("id", type=int, required=True)
+deviceParser.add_argument("name", type=str, required=True)
+deviceParser.add_argument("type", type=str, required=True)
+deviceParser.add_argument("network_ssid", type=str, required=True)
+deviceParser.add_argument("ip", type=str, required=True)
+deviceParser.add_argument("id_user", type=int, required=True)
+
+class Device(Resource):
+    @jwt_required()
+    def get(self, id):
+        pass
+
+    @jwt_required()
+    def post(self, id):
+        try:
+            args = deviceParser.parse_args()
+            device = db.Device(name=args["name"], type=args["type"], network_ssid=args["network_ssid"], ip=args["ip"], id_user=args["id_user"])
+            db.session.add(device)
+            db.session.commit()
+            return {"message": "Device created successfully"}, 200
+        except:
+            return {"message": "Invalid data"}, 400 
+
+    @jwt_required()
+    def delete(self, id):
+        pass
+
+    @jwt_required()
+    def put(self, id):
+        pass
+
+class DeviceList(Resource):
+    @jwt_required()
+    def get(self):
+        return list(map(lambda x: x.as_dict(), db.session.query(db.Device).filter(db.Device.id_user == get_jwt_identity()["id"]).all()))
 
 authParser = reqparse.RequestParser()
 authParser.add_argument("password", required=True)
@@ -24,8 +63,8 @@ class Auth(Resource):
     def post(self):
         try:
             args = authParser.parse_args()
-        except:
-            return {"error": "Incorrect arguments provided"}, 342
+        except Exception as e:
+            return {"error": str(e)}, 342
 
         try:
             user = db.session.query(db.User).filter(
@@ -33,7 +72,8 @@ class Auth(Resource):
             key = hashlib.pbkdf2_hmac('sha256', args["password"].encode(
                 "utf-8"), user.salt.encode("utf-8"), 100000).hex()
             if key == user.password:
-                return {"token": create_access_token(identity=user.username, expires_delta=False)}, 200
+                print(user.as_dict())
+                return {"token": create_access_token(identity=user.as_dict(), expires_delta=False)}, 200
             else:
                 return {"error": "Incorrect username or password"}, 342
         except Exception as e:
@@ -42,8 +82,8 @@ class Auth(Resource):
     def put(self):
         try:
             args = authParser.parse_args()
-        except:
-            return {"error": "Incorrect arguments provided"}, 342
+        except Exception as e:
+            return {"error": e.__str__()}, 342
 
         try:
             user = db.session.query(db.User).filter(
@@ -63,6 +103,8 @@ class Auth(Resource):
             return {"error": str(e)}, 342
 
 api.add_resource(Auth, "/v1/auth")
+api.add_resource(Device, "/v1/device/<int:id>")
+api.add_resource(DeviceList, "/v1/device_list")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
