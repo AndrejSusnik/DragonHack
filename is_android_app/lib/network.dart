875a5e99924c63f7e192993ab6e1d7c47c0d70c2
MrSlairy <72517116+MrSlairy@users.noreply.github.com>
2022-05-15 00:20:19 +0200

    commit is logscreen

---
+++ is_android_app/lib/network.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:is_android_app/ip_adress.dart';
+
+class Network {
+  late final IPAddress networkAddress;
+  late final IPAddress maxAddress;
+
+  Network(InetAddress address, short mask) {
+    late IPAddress ip = new IPAddress()
+  }
+}
