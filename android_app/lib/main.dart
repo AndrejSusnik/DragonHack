4cf9607a2edfac0fef023d2fc97865f1aaa12f7d
zrezke <jeretina.filip@gmail.com>
2022-05-14 14:54:38 +0200

    http server example

---
+++ android_app/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String statusText = "Start Server";
+  startServer() async {
+    setState(() {
+      statusText = "Starting server on Port : 8080";
+    });
+    // var chain =
+    //     Platform.script.resolve('certificates/server_chain.pem').toFilePath();
+    // var key =
+    //     Platform.script.resolve('certificates/server_key.pem').toFilePath();
+    // var context = SecurityContext()
+    //   ..useCertificateChain(chain)
+    //   ..usePrivateKey(key, password: 'dartdart');
+    var server = await HttpServer.bind("0.0.0.0", 9999);
+    print("Server running on IP : " +
+        server.address.toString() +
+        " On Port : " +
+        server.port.toString());
+    await for (var request in server) {
+      request.response
+        ..headers.contentType =
+            new ContentType("text", "plain", charset: "utf-8")
+        ..write('Hello, world')
+        ..close();
+    }
+    setState(() {
+      statusText = "Server running on IP : " +
+          server.address.toString() +
+          " On Port : " +
+          server.port.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              startServer();
+            },
+            child: Text(statusText),
+          )
+        ],
+      ),
+    ));
+  }
+}
