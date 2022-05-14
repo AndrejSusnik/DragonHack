f4c1954724dc27628a9a0a6b1dc91892c8b7caf4
Andrej susnik <susnik.andrej7@gmail.com>
2022-05-14 13:53:56 +0200

    Add working pc app

---
+++ PCApp2/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.pins.pcapp2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.pins.pcapp2 to javafx.fxml;
+    exports com.pins.pcapp2;
+}
